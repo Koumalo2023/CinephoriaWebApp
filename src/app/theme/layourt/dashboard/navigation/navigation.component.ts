0fb42dbecbaf96ae5eb2e093f25483ab211e10c3
Patrice SIMO <simopatrice@yahoo.fr>
2024-10-06 12:24:13 +0200

    #530 Mise a jour de navComponents

--- src/app/theme/layourt/dashboard/navigation/navigation.component.ts
+++ src/app/theme/layourt/dashboard/navigation/navigation.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
-import { ThemeService } from '../../../shared/services/ThemeService';
 import { NAVIGATION_ITEMS, NavigationItem } from './navigation';
 
 @Component({
