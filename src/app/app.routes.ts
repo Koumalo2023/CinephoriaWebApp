cc94315a2b7073398517861dff31c0add94ab0fc
Patrice SIMO <simopatrice@yahoo.fr>
2024-10-06 12:13:27 +0200

    AB#526 Création du module et des routing Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,39 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './theme/layourt/dashboard/dashboard.component';
+import { AuthComponent } from './theme/layourt/auth/auth.component';
+import { HomeComponent } from './theme/layourt/home/home.component';
 
 export const routes: Routes = [
-    { path: 'dashboard', component: DashboardComponent },
+    {
+      path: 'auth',
+      component: AuthComponent,
+      children: [
+        {
+          path: '',
+          loadChildren: () => import('../app/core/routes/authRoute/auth.module').then((m) => m.AuthModule)
+        }
+      ]
+    },
+    {
+      path: 'home',
+      component: HomeComponent,
+      children: [
+        {
+          path: '',
+          loadChildren: () => import('../app/core/routes/homeRoute/home.module').then((m) => m.HomeModule)
+        }
+      ]
+    },
+    {
+      path: 'default',
+      component: DashboardComponent,
+      children: [
+        {
+          path: '',
+          loadChildren: () => import('../app/core/routes/dashboardRoute/dashboard-route.module').then((m) => m.DashboardRouteModule)
+        }
+      ]
+    },
     // { path: 'profile', component: ProfileComponent },
     // { path: 'settings', component: SettingsComponent, 
     //   children: [
